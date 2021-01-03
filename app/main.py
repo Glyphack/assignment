c37ea466f99be3b6b7d30a0149023670fbe45c45
Glyphack <sh.hooshyari@gmail.com>
2021-01-03 21:24:14 +0330

    Add static content(csv files and images) (#2)
    * Add images and csv files
    
    * Create a route to serve static images

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
-
 from app.server.routes import api_router
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="campaign_banner", version="0.1.0")
 
+app.mount("/static", StaticFiles(directory="static/images"), name="static")
 app.include_router(api_router)
