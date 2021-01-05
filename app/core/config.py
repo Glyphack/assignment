0bd3a8ec7c72c3e3eceb3ca07caaeb3ed9ad1a0f
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-01-05 18:59:03 +0330

    Fix BASEDIR wrong path

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
-load_dotenv(dotenv_path=os.path.join(BASEDIR, '../.env'))
+load_dotenv(dotenv_path=os.path.join(BASEDIR, '.env'))
 
-CSV_FILES_PATH = os.path.join(os.path.dirname(BASEDIR), '../static/csv')
+CSV_FILES_PATH = os.path.join(os.path.dirname(BASEDIR), '/static/csv')
 
 STATIC_CONTENT_BASE_URL = os.getenv("STATIC_CONTENT_URL")
