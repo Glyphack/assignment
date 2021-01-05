f4e1e59b90e612d735661989c94070889ae614f5
Shayegan Hooshyari <sh.hooshyari@gmail.com>
2021-01-05 18:58:46 +0330

    Return template response in camplagn endpoint

--- app/server/endpoints/campaign.py
+++ app/server/endpoints/campaign.py
@@ -1,8 +1,13 @@
+import random
+
+from app.services.campaign.banner_chooser import BannerChooser
+from app.services.campaign.campaign_data import (
+    get_campaign_all_banners_data_frame,
+)
+from app.services.static_files import banner_image
+from app.services.templates.template import render_to_string
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-from app.services.campaign.banner_chooser import BannerChooser
-from app.services.static_files import banner_image
-from app.services.campaign.campaign_data import get_campaign_all_banners_data_frame
 
 router = APIRouter()
 
@@ -11,7 +16,9 @@ router = APIRouter()
 async def get_campaign_banners(request: Request, campaign_id):
     all_banners = await get_campaign_all_banners_data_frame(campaign_id, 1)
     banner_chooser = BannerChooser(all_banners)
-    banners = ""
-    for banner in banner_chooser.choose_banners():
-        banners += banner_image.get_banner_image_url(banner)
-    return banners
+    banners = banner_chooser.choose_banners()
+    banner_images = [
+        banner_image.get_banner_image_url(banner) for banner in banners
+    ]
+    random.shuffle(banners)
+    return render_to_string('campaign.html', banners=banner_images)
